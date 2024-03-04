7b7427d4611d0ec7ac71d9bea1362583081959be
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2024-03-03 19:28:48 -0800

    add scoring

--- src/Levels/Score.cs
+++ src/Levels/Score.cs
@@ -6,6 +6,9 @@ public partial class Score : Node2D
 	// Called when the node enters the scene tree for the first time.
 	[Export] 
 	public Label Refrain;
+
+	[Export] 
+	public RichTextLabel ScoreLabel;
 	
 	public override void _Ready()
 	{
@@ -14,6 +17,24 @@ public partial class Score : Node2D
 		if (Score < 100)
 		{
 			Refrain.Visible = true;
+		}
+
+		ScoreLabel.Text = "[center]Score: " + Score + "%, Grade: ";
+
+		switch (Score)
+		{
+			case (>=90):
+				ScoreLabel.Text = ScoreLabel.Text + "A";
+				break;
+			case (>=80):
+				ScoreLabel.Text = ScoreLabel.Text + "B";
+				break;
+			case (>=70):
+				ScoreLabel.Text = ScoreLabel.Text + "C";
+				break;
+			case (>=60):
+				ScoreLabel.Text = ScoreLabel.Text + "D";
+				break;
 		}
 	}
 
