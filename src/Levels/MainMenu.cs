ede704a3e08c5a373f3ca94ada75b6e91055c98d
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2024-03-08 18:34:44 -0800

    add main menu and disable grass shadows

---
+++ src/Levels/MainMenu.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	[Export] 
+	private PackedScene FirstLevel;
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void Quit()
+	{
+		GetTree().Quit();
+	}
+
+	public void Start()
+	{
+		GetTree().ChangeSceneToPacked(FirstLevel);
+	}
+}
